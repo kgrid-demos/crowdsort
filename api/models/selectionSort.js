4d963c71e8eb3115eec6e6f07bd391569bcb0bc9
skurosu <skurosu@umich.edu>
2019-01-11 16:11:55 -0500

    accounts for all selection events

--- api/models/selectionSort.js
+++ api/models/selectionSort.js
@@ -5,8 +5,13 @@ const sortSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
   // The type and required syntax ensures that the user enters the data type
   date: {type: Number, required: true},
-  hasExpectedSolution: {type: String , required: true}
-
+  time:{type:Number, required: true},
+  hasExpectedSolution: {type: String , required: true},
+  expectedSolution: {type: String , required: true},
+  expectedSelectionConfidence: {type: String , required: true},
+  userSelection: {type: String , required: true},
+  userSelectionConfidence: {type: String , required: true},
+  notSelected:{type: String , required: true}
 });
 
 module.exports = mongoose.model('SelectionSort',sortSchema);
