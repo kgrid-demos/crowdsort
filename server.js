73879a7c0ab2e5182df4ae1946297ad9da958799
circleci <kgrid-developers@umich.edu>
2019-01-27 22:18:35 -0500

    Updated mongodb parms

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ console.log('server started '+ port);
 
 const selectionSortRoutes = require('./api/routes/selectionSort');
 
-mongoose.connect('mongodb://127.0.0.1:27017/test?retryWrites=true',{
+mongoose.connect( process.env.MONGODB_URI ||'mongodb://127.0.0.1:27017/test?retryWrites=true',{
   useNewUrlParser: true,
 
 })
