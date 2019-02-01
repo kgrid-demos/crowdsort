fdf6c7c89506f46f834974d0db80eeae4942aa5c
Jack Allan <jackallan@Kathleens-MacBook-Pro-2.local>
2019-02-01 11:42:36 -0500

    fixed post body

--- api/models/selectionSort.js
+++ api/models/selectionSort.js
@@ -6,6 +6,7 @@ const sortSchema = mongoose.Schema({
   // The type and required syntax ensures that the user enters the data type
   date: {type:String, required: true},
   time:{type:String, required: true},
+  comparison: {type:String, required: true},
   userSelected: {type:String, required:true},
   userNotSelected: {type:String, required:true}
 });
